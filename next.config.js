d68acd0bf2fd3c5f4d23e17034b985372f0f6a89
Usman Abdur Rehman <saidskit@outlook.com>
2024-02-11 13:05:02 +0500

    Edited next config alias

--- next.config.js
+++ next.config.js
@@ -3,11 +3,7 @@ const path = require("path");
 
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "@": path.resolve(__dirname, "./"),
-    };
-
+    config.resolve.alias["@"] = path.join(__dirname, "src");
     return config;
   },
 };
